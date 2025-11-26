45bdb80ed1ce691a3fe92066b79298f15931f302
Lucas Alves <lucas1901alves@gmail.com>
2025-11-26 00:18:31 -0300

    feat: add api.ts

---
+++ src/lib/api.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+    
+    // withCredentials: true, AJUSTAR AQUI PARA USAR COOKIES MAIS TARDE
+})
