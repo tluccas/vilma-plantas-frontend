b1445ac4e4b09697111526f840780ad6693cbb23
Lucas Alves <lucas1901alves@gmail.com>
2025-11-27 01:35:45 -0300

    refactor(component): refactor label and input style

--- src/features/auth/components/InputField.tsx
+++ src/features/auth/components/InputField.tsx
@@ -7,10 +7,10 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
 export default function InputField({ label, ...rest}: Props){
 
     return (
-        <div className="flex flex-col gap-1">
-            <label className="text-sm font-medium text-gray-700">{label}</label>
+        <div className="flex flex-col gap-1 w-full">
+            <label className="text-lg mt-5! font-medium text-gray-700">{label}</label>
             <input {...rest} 
-              className="w-full border border-gray-300 rounded-md px-3 py-2 text-sm outline-none focus:ring focus:ring-green-500 focus:border-green-500"
+              className="w-full h-10 border border-gray-300 rounded-md px-3 p-2! text-sm outline-none focus:ring focus:ring-green-500 focus:border-green-500"
               />
         </div>
     );
