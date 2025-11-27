cca5fb938998916b1420fa3766ad2cc4f9b3bafc
Lucas Alves <lucas1901alves@gmail.com>
2025-11-27 02:29:20 -0300

    style(margin): rm margin top

--- src/features/auth/components/InputField.tsx
+++ src/features/auth/components/InputField.tsx
@@ -8,7 +8,7 @@ export default function InputField({ label, ...rest}: Props){
 
     return (
         <div className="flex flex-col gap-1 w-full">
-            <label className="text-lg mt-5! font-medium text-gray-700">{label}</label>
+            <label className="text-lg font-medium text-gray-700">{label}</label>
             <input {...rest} 
               className="w-full h-10 border border-gray-300 rounded-md px-3 p-2! text-sm outline-none focus:ring focus:ring-green-500 focus:border-green-500"
               />
