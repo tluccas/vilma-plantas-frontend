748687c8434c3461cae438428b9b5f84c3795d84
Lucas Alves <lucas1901alves@gmail.com>
2025-11-29 15:12:20 -0300

    feat(route): add protected route

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import type { PropsWithChildren } from "react";
+import { useAuth } from "../features/auth/hooks/useAuth.ts";
+import { Navigate } from "react-router-dom";
+import Loading from "../pages/Loading.tsx";
+
+type PropsWithChildrenProps = PropsWithChildren;
+
+function ProtectedRoute({ children }: PropsWithChildrenProps) {
+    const { isAuthenticated, isInitialized } = useAuth();
+
+    if(!isInitialized) {
+        return <Loading />;
+    }
+
+    if(!isAuthenticated) {
+        return <Navigate to="/login" />;
+    }
+
+    return children;
+}
