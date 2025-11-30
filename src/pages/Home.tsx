3bfef3bca1161ada0d2d5f84e2cae082c966dc52
Lucas Alves <lucas1901alves@gmail.com>
2025-11-30 20:22:43 -0300

    feat(page): add page tittle using Helmet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,18 @@
 import Header from "../components/layout/Header";
 import Hero from "../components/layout/home/Hero";
-
+import { Helmet } from "react-helmet-async";
 export default function Home() {
     return (
-        <div className="min-h-screen overflow-x-hidden" style={{ backgroundColor: '#ebebeb' }}>
-            <Header />
-            <main className="py-20">
-                <Hero />
-            </main>
-        </div>
+        <>
+            <Helmet>
+                <title>Home | Vilma Plantas</title>
+            </Helmet>
+            <div className="min-h-screen overflow-x-hidden" style={{ backgroundColor: '#ebebeb' }}>
+                <Header />
+                <main className="py-20">
+                    <Hero />
+                </main>
+            </div>
+        </>
     );
 }
