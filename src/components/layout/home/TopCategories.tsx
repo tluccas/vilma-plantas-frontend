9311c435e29269acb6cab12111f2f35d00adc72c
Lucas Alves <lucas1901alves@gmail.com>
2025-12-01 01:27:23 -0300

    refactor: migrate TopCategories to use local images

--- src/components/layout/home/TopCategories.tsx
+++ src/components/layout/home/TopCategories.tsx
@@ -7,19 +7,19 @@ export default function TopCategories() {
       <div className="categories-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 mt-6 justify-center">
         <CategoriesCards 
           title="Orquídeas" 
-          img="https://images.unsplash.com/photo-1632011433298-689d8a6135a4?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          img="/images/plants/orquidea-card.jpg" 
         />
         <CategoriesCards 
           title="Suculentas" 
-          img="https://images.unsplash.com/photo-1551893665-f843f600794e?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          img="/images/plants/suculentas-card.jpg" 
         />
         <CategoriesCards 
           title="Samambaias" 
-          img="https://images.unsplash.com/photo-1760184347560-f7463e36859a?q=80&w=435&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          img="/images/plants/samambaia-card.jpg" 
         />
         <CategoriesCards 
-          title="Suculentas" 
-          img="https://images.unsplash.com/photo-1551893665-f843f600794e?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          title="Rosas" 
+          img="/images/plants/rose-card.jpg" 
         />
       </div>
     </section>
