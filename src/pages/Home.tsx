93a79f5c5cb25ae4db2fa80ae50177f3e5de2598
Lucas Alves <lucas1901alves@gmail.com>
2025-11-30 23:30:18 -0300

    feat(bg): background set to use default variable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,20 @@
 import Header from "../components/layout/Header";
 import Hero from "../components/layout/home/Hero";
+import TopCategories from "../components/layout/home/TopCategories";
 import { Helmet } from "react-helmet-async";
+
 export default function Home() {
     return (
         <>
             <Helmet>
                 <title>Home | Vilma Plantas</title>
             </Helmet>
-            <div className="min-h-screen overflow-x-hidden" style={{ backgroundColor: '#ebebeb' }}>
+            <div className="min-h-screen overflow-x-hidden bg-(--color-background)">
                 <Header />
                 <main className="py-20">
-                    <Hero />
+                        <Hero />  
+                        <TopCategories />
+                    
                 </main>
             </div>
         </>
