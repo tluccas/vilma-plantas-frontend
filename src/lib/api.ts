34d4e6ff5a5db1faa1e88fa1ddb7edee6118c937
Lucas Alves <lucas1901alves@gmail.com>
2025-12-01 01:05:38 -0300

    refactor(url): Refactor base URL API

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: import.meta.env.SITE_API_URL,
   withCredentials: true, // Habilita cookies para todas as requisições
 });
