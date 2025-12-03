91709f0f7b43b182bdeb343a8c118503a2e69ea7
Lucas Alves <lucas1901alves@gmail.com>
2025-12-03 17:35:28 -0300

    feat(types): add productTypes Response and Model

---
+++ src/features/products/types/ProductType.ts
@@ -0,0 +1,32 @@
+export interface ProductResponse {
+  id: number;
+  name: string;
+  price: string;
+  description: string;
+  category_id: number;
+  stock: number;
+  createdAt: string;
+  updatedAt: string;
+  category: {
+    name: string;
+  };
+}
+
+export interface ProductTypeResponse {
+  products: ProductResponse[];
+  total: number;
+}
+
+export interface ProductModel {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  category_id: number;
+  stock: number;
+  createdAt: string;
+  updatedAt: string;
+  category: {
+    name: string;
+  };
+}
