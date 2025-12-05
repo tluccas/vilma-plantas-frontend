bed32bb73ac68f77a31b8b52c09c8f52691c6106
Lucas Alves <lucas1901alves@gmail.com>
2025-12-05 01:39:20 -0300

    refactor(component): refactors stroke properties to camelCase

--- src/features/products/components/cards/ProductCards.tsx
+++ src/features/products/components/cards/ProductCards.tsx
@@ -40,9 +40,9 @@ export function ProductCards({ product, redirectUrl }: ProductCardProps) {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M19 12H5m14 0-4 4m4-4-4-4"
               />
             </svg>
