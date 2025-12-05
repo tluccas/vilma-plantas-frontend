01fbe09068ae8f37ee2ce6891f5b49e31b84adc4
Lucas Alves <lucas1901alves@gmail.com>
2025-12-05 01:38:35 -0300

    refactor(component): refactors stroke properties to camelCase

--- src/components/layout/home/cards/CategoriesCards.tsx
+++ src/components/layout/home/cards/CategoriesCards.tsx
@@ -27,7 +27,7 @@ export default function TopCategories({ title, img, redirectUrl }: TopCategories
                 {/* Redirecionar para /produtos filtrando a categoria */}
                 <a href={redirectUrl} className="inline-flex items-center text-white! bg-(--button-primary-bg) box-border border border-transparent rounded-3xl hover:bg-(--button-secondary-hover-bg) focus:ring-4 focus:ring-brand-medium shadow-xl font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none transition duration-300 ease-in-out select-none">
                     Ver Produtos
-                    <svg className="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4"/></svg>
+                    <svg className="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 12H5m14 0-4 4m4-4-4-4"/></svg>
                 </a>
             </div>
         </div>
