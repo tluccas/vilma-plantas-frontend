3c08a2fadd34b202f0992390fbec7fae85c7cf54
Lucas Alves <lucas1901alves@gmail.com>
2026-01-04 17:45:55 -0300

    feat(products): add CMS to temporary site version

--- src/components/layout/home/HomeProducts.tsx
+++ src/components/layout/home/HomeProducts.tsx
@@ -1,27 +1,16 @@
-// import { ProductService } from "../../../features/products/api/ProductServie";
-import { mockProducts } from "../../../features/products/mocks/mockProduct";
 import { ProductCards } from "../../../features/products/components/cards/ProductCards";
+import { ProductService } from "../../../features/products/api/ProductServie";
 
-// const productService = new ProductService();
 
 export default function HomeProducts() {
-    // Usando mocks de produtos por enquanto até a API estar em produção
-    // const [products, setProducts] = useState<ProductModel[]>([]);
 
-    // useEffect(() => {
-    //     async function loadProducts(){
-    //         const res = await productService.getProducts(1);
-    //         setProducts(res);
-    //     }
-
-    //     loadProducts();
-    // }, []);
+        const products = ProductService.getProducts();
   
         return (
            <section className="top-categories px-4 sm:px-6 lg:px-8 py-6">
                 <h2 className="mt-2 mb-10 text-xl sm:text-2xl font-semibold text-heading text-center">Vitrine <span className="text-(--color-primary) font-bold">Botânica</span></h2>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 mt-6 justify-center">
-                            { mockProducts.map(prd => (
+                            { products.map(prd => (
                                 <ProductCards 
                                 key={prd.id} 
                                 product={prd}
