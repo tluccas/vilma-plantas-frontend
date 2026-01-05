ab6796f5c26e629d820ab18d47bb17643ca537e5
Lucas Alves <lucas1901alves@gmail.com>
2026-01-05 15:32:32 -0300

    chore(dependency): add react toast dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { AuthProvider } from "@/features/auth/contexts/AuthProvider";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "Vilma Plantas",
   description: "Sua loja de plantas online",
   icons: {
     icon: "/favicon.svg",
-  }
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +21,9 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body>
         <AuthProvider>{children}</AuthProvider>
+
+        {/* Toaster para notificações */}
+        <Toaster position="top-right" />
       </body>
     </html>
   );
